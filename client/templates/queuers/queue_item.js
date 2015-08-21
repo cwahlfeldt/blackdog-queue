e7d3760b0fed5e2b40b91e20710f7af3e4f5c210
cwahlfeldt <chriswahlfeldt@gmail.com>
2015-08-20 22:19:45 -0500

    hotfix

--- client/templates/queuers/queue_item.js
+++ client/templates/queuers/queue_item.js
@@ -57,47 +57,52 @@ Template.queueItem.events({
             _id: this._id
         }, {});
 
-        if (confirm('Are you sure you want to seat the customer?') && queuer.phoneNumber === '') {
+        if (confirm('Are you sure you want to seat the customer?')) {
             if (queuer._id === '') {
                 return sAlert.error('Cant find queuer!');
             }
+            
+            // no need to message the queuer if no phone number has been placed
+            if (queuer.phoneNumber !== '') {
+                var message = 'Your table for ' +
+                    queuer.partySize + ' is ready ' +
+                    queuer.name + '. Come on in!';
 
-            var message = 'Your table for ' +
-                queuer.partySize + ' is ready ' +
-                queuer.name + '. Come on in!';
-
-            var data = {
-                queuer: queuer,
-                message: message
-            };
-
-            // seat the customer and call the message method
-            Meteor.call('messageQueuer', data, function(error, result) {
-                if (error) {
-                    return sAlert.error(error);
-                }
-
-                // get hours
-                var fromHours = result.date.getHours(),
-                    toHours = new Date().getHours();
-                // get min
-                var fromMin = result.date.getMinutes(),
-                    toMin = new Date().getMinutes();
-                // get both ti mes in a nice little object literal
-                var waitTime = {
-                    hours: toHours - fromHours,
-                    min: toMin - fromMin
+                var data = {
+                    queuer: queuer,
+                    message: message
                 };
 
-                // reset to false to re-up the helper 
-                Session.set('waitTimeBool', false);
+                // seat the customer and call the message method
+                Meteor.call('messageQueuer', data, function(error, result) {
+                    if (error) {
+                        return sAlert.error(error);
+                    }
 
-                waitTimes.push(waitTime.min);
+                    // get hours
+                    var fromHours = result.date.getHours(),
+                        toHours = new Date().getHours();
+                    // get min
+                    var fromMin = result.date.getMinutes(),
+                        toMin = new Date().getMinutes();
+                    // get both ti mes in a nice little object literal
+                    var waitTime = {
+                        hours: toHours - fromHours,
+                        min: toMin - fromMin
+                    };
 
-                if (waitTimes.length >= 1) {
-                    Session.set('waitTimeBool', true);
-                }
-            });
+                    // reset to false to re-up the helper 
+                    Session.set('waitTimeBool', false);
+
+                    waitTimes.push(waitTime.min);
+
+                    if (waitTimes.length >= 1) {
+                        Session.set('waitTimeBool', true);
+                    }
+                });
+            }
+            
+            // remove queuer from line after being sent a message
             Meteor.call('queuerRemove', this._id, function(error, result) {
                 if (error) {
                     return sAlert.error('Remove Error!');
